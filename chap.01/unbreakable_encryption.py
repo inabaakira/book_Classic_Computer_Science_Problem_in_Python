87d5294f44f4cf5ac7fd34415ce8855ce22dada1
INABA Akira <inaba.akira@gmail.com>
2019-07-27 11:12:10 +0000

    [Chap.01] Listing 1.16. unbreakable_encryption.py continued

--- chap.01/unbreakable_encryption.py
+++ chap.01/unbreakable_encryption.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: unbreakable_encryption.py
 #    Created:       <2019/07/25 00:51:35>
-#    Last Modified: <2019/07/25 00:53:39>
+#    Last Modified: <2019/07/27 15:18:53>
 
 from secrets import token_bytes
 from typing import Tuple
@@ -12,3 +12,10 @@ def random_key(length: int) -> int:
     tb: bytes = token_bytes(length)
     # convert those bytes into a bit string and return it
     return int.from_bytes(tb, "big")
+
+def encrypt(original: str) -> Tuple[int, int]:
+    original_bytes: bytes = original.encode()
+    dummy: int = random_key(len(original_bytes))
+    original_key: int = int.from_bytes(original_bytes, "big")
+    encrypted: int = original_key ^ dummy # XOR
+    return dummy, encrypted
