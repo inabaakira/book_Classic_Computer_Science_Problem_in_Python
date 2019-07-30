eb10a0dee9a3fc4b24553bd8b887d693a9c97c8a
INABA Akira <inaba.akira@gmail.com>
2019-07-30 11:20:09 +0000

    [Chap.01] Listing 1.21. hanoi.py continued

--- chap.01/hanoi.py
+++ chap.01/hanoi.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: hanoi.py
 #    Created:       <2019/07/28 20:07:50>
-#    Last Modified: <2019/07/28 20:10:19>
+#    Last Modified: <2019/07/30 20:21:49>
 
 from typing import TypeVar, Generic, List
 T = TypeVar('T')
@@ -20,3 +20,10 @@ class Stack(Generic[T]):
 
     def __repr__(self) -> str:
         return repr(self._container)
+
+num_discs: int = 3
+tower_a: Stack[int] = Stack()
+tower_b: Stack[int] = Stack()
+tower_c: Stack[int] = Stack()
+for i in range(1, num_discs + 1):
+    tower_a.push(i)
