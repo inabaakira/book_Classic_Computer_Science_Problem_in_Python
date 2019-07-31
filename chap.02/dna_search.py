ede4c75607d55cd145d0602a20dde3d82836c6c4
INABA Akira <inaba.akira@gmail.com>
2019-07-31 12:21:31 +0000

    [Chap.02] Listing 2.2. dna_search.py continued

--- chap.02/dna_search.py
+++ chap.02/dna_search.py
@@ -2,9 +2,11 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: dna_search.py
 #    Created:       <2019/07/31 20:40:40>
-#    Last Modified: <2019/07/31 21:20:44>
+#    Last Modified: <2019/07/31 21:21:24>
 
 from enum import IntEnum
 from typing import Tuple, List
 
 Nucleotide: IntEnum = IntEnum('Nucleotide', ('A', 'C', 'G', 'T'))
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide] # type alias for codons
+Gene = List[Codon] # type alias for genes
