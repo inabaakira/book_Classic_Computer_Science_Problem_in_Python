27bf6bae3ee5824b6a282b26e45af25e8689a8bf
INABA Akira <inaba.akira@gmail.com>
2019-08-03 09:06:50 +0000

    [Chap.02] Listing 2.4. dna_search.py continued

--- chap.02/dna_search.py
+++ chap.02/dna_search.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: dna_search.py
 #    Created:       <2019/07/31 20:40:40>
-#    Last Modified: <2019/08/02 21:09:52>
+#    Last Modified: <2019/08/03 10:30:22>
 
 from enum import IntEnum
 from typing import Tuple, List
@@ -12,3 +12,13 @@ Codon = Tuple[Nucleotide, Nucleotide, Nucleotide] # type alias for codons
 Gene = List[Codon] # type alias for genes
 
 gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGGTTTATATATACCCTAGGACTCCCTTT"
+
+def string_to_gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if (i + 2) >= len(s): # don't run off end!
+            return gene
+        # initialize codon out of three nuleotides
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
+        gene.append(codon) # add codon to gene
+    return gene
