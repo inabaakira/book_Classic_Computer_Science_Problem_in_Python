076a33be650f57b123bc1e644178e6d631f081c9
INABA Akira <inaba.akira@gmail.com>
2019-08-21 08:20:18 +0000

    [Chap.02] Listing 2.10. maze.py

---
+++ chap.02/maze.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#-*- mode: python; coding: utf-8 -*-
+# file: maze.py
+#    Created:       <2019/08/21 17:09:42>
+#    Last Modified: <2019/08/21 17:18:29>
+
+from enum import Enum
+from typing import List, NamedTuple, Callable, Optional
+import random
+from math import sqrt
+# from generic_search import dfs, bfs, node_to_path, astar, Node
+
+class Cell(str, Enum):
+    EMPTY = " "
+    BLOCKED = "X"
+    START = "S"
+    GOAL = "G"
+    PATH = "*"
