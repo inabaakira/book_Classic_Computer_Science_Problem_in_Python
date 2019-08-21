7b33850cceaad4e5bc9f4dde772dd62f5f9dcdf7
INABA Akira <inaba.akira@gmail.com>
2019-08-21 09:11:31 +0000

    [Chap.02] Listing 2.12. maze.py continued

--- chap.02/maze.py
+++ chap.02/maze.py
@@ -20,3 +20,27 @@ class Cell(str, Enum):
 class MazeLocation(NamedTuple):
     row: int
     column: int
+
+class Maze:
+    def __init__(self, rows: int = 10, columns: int = 10, sparseness: float = 0.2,
+                 start: MazeLocation = MazeLocation(0, 0),
+                 goal: MazeLocation = MazeLocation(9, 9)) -> None:
+        # initialize basic instance variables
+        self._rows: int = rows
+        self._columns: int = columns
+        self.start: MazeLocation = start
+        self.goal: MazeLocation = goal
+        # fill the grid with empty cells
+        self._grid: List[List[Cell]] = [[Cell.EMPTY for c in range(columns)] for r in range(rows)]
+        # populate the grid with blocked cells
+        self._randomly_fill(rows, columns, sparseness)
+        # fill the start and goal locations in
+        self._grid[start.row][start.column] = Cell.START
+        self._grid[goal.row][goal.column] = Cell.GOAL
+
+    def _randomly_fill(self, rows: int, columns: int, sparseness: float):
+        for row in range(rows):
+            for column in range(columns):
+                if random.uniform(0, 1.0) < sparseness:
+                    self._grid[row][column] = Cell.BLOCKED
+
