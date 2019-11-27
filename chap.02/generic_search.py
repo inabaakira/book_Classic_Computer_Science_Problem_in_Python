fd800b3500f1a6453761d8e37153c318be606b1f
INABA Akira <inaba@inabaakira.org>
2019-11-27 11:05:09 +0900

    [Chap.02] Listing 2.18. generic_search.py continued

--- chap.02/generic_search.py
+++ chap.02/generic_search.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: generic_search.py
 #    Created:       <2019/08/05 14:33:37>
-#    Last Modified: <2019/08/22 21:14:14>
+#    Last Modified: <2019/11/27 11:04:06>
 
 from __future__ import annotations
 from typing import TypeVar, Iterable, Sequence, Generic, List, Callable, Set, \
@@ -75,6 +75,29 @@ def binary_contains(sequence: Sequence[C], key: C) -> bool:
         else:
             return True
     return False
+
+def dfs(initial: T, goal_test: Callable[[T], bool],
+        successors: Callable[[T], List[T]]) -> Optional[Node[T]]:
+    # frontier is where we've not yet to go
+    frontier: Stack[Node[T]] = Stack()
+    frontier.push(Node(initial, None))
+    # explored is where we've been
+    explored: Set[T] = {initial}
+
+    # keep going while there is more to explore
+    while not frontier.empty:
+        current_node: Node[T] = frontier.pop()
+        current_state: T = current_node.state
+        # if we found the goal, we're done
+        if goal_test(current_state):
+            return current_node
+        # check where we can go next and haven't explored
+        for child in successors(current_state):
+            if child in explored: # skip children we already explored
+                continue
+            explored.add(child)
+            frontier.push(Node(child, current_node))
+    return None # went through everything and never found goal
 
 if __name__ == "__main__":
     print(linear_contains([1, 5, 15, 15, 15, 15, 20], 5)) # True
