60e8c43ba9f8dd840e6a7b31a68b00153b4a1ea2
INABA Akira <inaba.akira@gmail.com>
2020-01-06 16:29:20 +0900

    [Chap.02] Listing 2.30. missionaries.py

---
+++ chap.02/missionaries.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#-*- mode: python; coding: utf-8 -*-
+# file: missionaries.py
+#    Created:       <2020/01/06 15:27:01>
+#    Last Modified: <2020/01/06 16:29:01>
+
+from __future__ import annotations
+from typing import List, Optional
+from generic_search import bfs, Node, node_to_path
+
+MAX_NUM: int = 3
+
+class MCState:
+    def __init__(self, missionaries: int, cannibals: int, boat: bool) -> None:
+        self.wm: int = missionaries # west bank missionaries
+        self.wc: int = cannibals # west bank cannibals
+        self.em: int = MAX_NUM # east bank missonaries
+        self.ec: int = MAX_NUM # east bank cannibals
+        self.boat: bool = boat
+
+    def __str__(self) -> str:
+        return ("On the west bank there are {} missionaries and {} cannibals.\n"
+                "On the east bank there are {} missionaries and {} cannibals.\n"
+                "The boat is on the {} bank.")\
+                .format(self.wm, self.wc, self.em, self.ec,
+                        ("west" if self.boat else "east"))
