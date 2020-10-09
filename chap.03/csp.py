0afc0e9278f37765a4dbb3963adc42991821c6e4
INABA Akira <inaba.akira@gmail.com>
2020-10-09 11:45:34 +0900

    [Chap.03] Listing 3.2. csp.py continued

--- chap.03/csp.py
+++ chap.03/csp.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: csp.py
 #    Created:       <2020/10/07 17:43:35>
-#    Last Modified: <2020/10/09 11:41:11>
+#    Last Modified: <2020/10/09 11:45:15>
 
 from typing import Generic, TypeVar, Dict, List, Optional
 from abc import ABC, abstractmethod
@@ -17,3 +17,20 @@ class Constraint(Generic[V, D], ABC):
     @abstractmethod
     def satisfied(self, assignment: Dict[V, D]) -> bool:
         ...
+
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
+        self.variables: List[V] = variables
+        self.domains: Dict[V, List[D]] = domains
+        self.constraints: Dict[V, List[V, D]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError("Every variable should have a domain assigned to it")
+
+    def add_constraint(self, constraint: Constraint[V, D]) -> None:
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("Variable in constraint not in CSP")
+            else:
+                self.constraints[variable].append(constraint)
