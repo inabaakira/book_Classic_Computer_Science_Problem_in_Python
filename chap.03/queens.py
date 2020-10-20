dbab22a1b0e103320fe024a01e6c74e457e3bd9f
INABA Akira <inaba.akira@gmail.com>
2020-10-20 17:09:57 +0900

    [Chap.03] Listing 3.9. queens.py continued

--- chap.03/queens.py
+++ chap.03/queens.py
@@ -2,7 +2,24 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: queens.py
 #    Created:       <2020/10/20 15:21:49>
-#    Last Modified: <2020/10/20 17:08:59>
+#    Last Modified: <2020/10/20 17:09:40>
+
+from csp import Constraint, CSP
+from typing import Dict, List, Optional
+class QueensConstraint(Constraint[int, int]):
+    def __init__(self, columns: List[int]) -> None:
+        super().__init__(columns)
+        self.columns: List[int] = columns
+    def satisfied(self, assignment: Dict[int, int]) -> bool:
+        for q1c, q1r in assignment.items():
+            for q2c in range(q1c + 1, len(self.columns) + 1):
+                if q2c in assignment:
+                    q2r: int = assignment[q2c]
+                    if q1r == q2r:
+                        return False
+                    if abs(q1r - q2r) == abs(q1c - q2c):
+                        return False
+        return True
 
 if __name__ == "__main__":
     columns: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
