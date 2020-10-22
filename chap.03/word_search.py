f30d1109ca504c86a543901f2c9943b378c8f448
INABA Akira <inaba.akira@gmail.com>
2020-10-22 17:52:22 +0900

    [Chap.03] Listing 3.13. word_search.py continued

--- chap.03/word_search.py
+++ chap.03/word_search.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: word_search.py
 #    Created:       <2020/10/22 13:58:58>
-#    Last Modified: <2020/10/22 15:14:54>
+#    Last Modified: <2020/10/22 17:05:48>
 
 from typing import NamedTuple, List, Dict, Optional
 from random import choice
@@ -21,3 +21,21 @@ def generate_grid(rows: int, columns: int) -> Grid:
 def display_grid(grid: Grid):
     for row in grid:
             print("".join(row))
+
+def generate_domain(word: str, grid: Grid) -> List[List[GridLocation]]:
+    domain: List[List[GridLocation]] = []
+    height: int = len(grid)
+    width: int = len(grid[0])
+    length: int = len(word)
+    for row in range(height):
+        columns: range = range(col, col + length + 1)
+        rows: range = range(row, row + length + 1)
+        if col + length <= width:
+           domain.append([GridLocation(row, c) for c in columns])
+           if row + length <= height:
+               domain.append([GridLocation(r, col + (r - row)) for r in rows])
+        if row + length <= height:
+            domain.append([GridLocation(r, col) for r in rows])
+            if col - length >= 0:
+                domain.append([GridLocation(r, col - (r - row)) for r in rows])
+    return domain
