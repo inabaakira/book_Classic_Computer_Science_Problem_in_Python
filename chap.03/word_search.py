1dacd7e1cbcd935948bf417192dbb4f59bbbeb89
INABA Akira <inaba.akira@gmail.com>
2020-10-22 15:15:15 +0900

    [Chap.03] Listing 3.12. word_search.py continued

--- chap.03/word_search.py
+++ chap.03/word_search.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: word_search.py
 #    Created:       <2020/10/22 13:58:58>
-#    Last Modified: <2020/10/22 14:01:29>
+#    Last Modified: <2020/10/22 15:14:54>
 
 from typing import NamedTuple, List, Dict, Optional
 from random import choice
@@ -14,3 +14,10 @@ Grid = List[List[str]]
 class GridLocation(NamedTuple):
     row: int
     column: int
+
+def generate_grid(rows: int, columns: int) -> Grid:
+    return [[choice(ascii_uppercase) for c in range(columns)] for r in range(rows)]
+
+def display_grid(grid: Grid):
+    for row in grid:
+            print("".join(row))
