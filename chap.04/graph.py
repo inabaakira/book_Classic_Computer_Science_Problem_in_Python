14285d95736c6b9febb729718705f80cb4c2d0da
INABA Akira <inaba.akira@gmail.com>
2021-01-12 09:03:40 +0900

    [Chap.04] Listing 4.3. graph.py continued

--- chap.04/graph.py
+++ chap.04/graph.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: graph.py
 #    Created:       <2021/01/10 11:23:20>
-#    Last Modified: <2021/01/10 11:25:44>
+#    Last Modified: <2021/01/11 15:33:22>
 
 from typing import TypeVar, Generic, List, Optional
 from edge import Edge
@@ -14,3 +14,53 @@ class Graph(Generic[V]):
     def __init__(self, vertices: List[V] = []) -> None:
         self._vertices: List[V] = vertices
         self._edges: List[List[Edge]] = [[] for _ in vertices]
+
+    @property
+    def vertex_count(self) -> int:
+        return len(self._vertices)
+
+    @property
+    def edge_count(self) -> int:
+        return sum(map(len, self._edges))
+
+    def add_vertex(self, vertex: V) -> int:
+        self._vertices.append(vertex)
+        self._edges.append([])
+        return self.vertex_count - 1
+
+    def add_edge(self, edge: Edge) -> None:
+        self._edges[edge.u].append(edge)
+        self._edges[edge.v].append(edge.reversed())
+
+    def add_edge_by_indices(self, u: int, v: int) -> None:
+        edge: Edge = Edge(u, v)
+        self.add_edge(edge)
+
+    def add_edge_by_vertices(sekf, first: V, second: V) -> None:
+        u: int = self._vertices.index(first)
+        v: int = self._vertices.index(second)
+        self.add_edge_by_indices(u, v)
+
+    def vertex_at(self, vertex: V) -> int:
+        return self._vertices[index]
+
+    def index_of(self, vertex: V) -> int:
+        return self._vertices.index(vertex)
+
+    def neighbors_for_index(self, index: int) -> List[V]:
+        return list(map(self.vertex_at, [e.v for e in self._edges[index]]))
+
+    def neighbors_for_vertex(self, vertex: V) -> List[V]:
+        return self.neighbors_for_index(self.index_of(vertex))
+
+    def edges_for_index(self, index: int) -> List[Edge]:
+        return self._edges[index]
+
+    def edges_for_vertex(self, vertex: V) -> List[Edge]:
+        return self.edges_for_index(self.index_of(vertex))
+
+    def __str__(self) -> str:
+        desc: str = ""
+        for i in range(self.vertex_count):
+            desc += f"{self.vertex_at(i) -> {self.neighbors_for_index(i)}}\n"
+        return desc
