6f4d41c5154e7e0aafa964d2e9ede009d9c1aa82
INABA Akira <inaba.akira@gmail.com>
2021-01-15 23:41:50 +0900

    [Chap.04] Listing 4.4. graph.py continued

--- chap.04/graph.py
+++ chap.04/graph.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: graph.py
 #    Created:       <2021/01/10 11:23:20>
-#    Last Modified: <2021/01/15 23:38:46>
+#    Last Modified: <2021/01/15 23:41:12>
 
 from typing import TypeVar, Generic, List, Optional
 from edge import Edge
@@ -64,3 +64,47 @@ class Graph(Generic[V]):
         for i in range(self.vertex_count):
             desc += f"{self.vertex_at(i)} -> {self.neighbors_for_index(i)}\n"
         return desc
+
+if __name__ == "__main__":
+    city_graph: Graph[str] = Graph(["Seattle",
+                                    "San Francisco",
+                                    "Los Angels",
+                                    "Riverside",
+                                    "Phoenix",
+                                    "Chicago",
+                                    "Boston",
+                                    "New York",
+                                    "Atlanta",
+                                    "Miami",
+                                    "Dallas",
+                                    "Houston",
+                                    "Detroit",
+                                    "Philadelphia",
+                                    "Washington"])
+    city_graph.add_edge_by_vertices("Seattle", "Chicago")
+    city_graph.add_edge_by_vertices("Seattle", "San Francisco")
+    city_graph.add_edge_by_vertices("San Francisco", "Riverside")
+    city_graph.add_edge_by_vertices("San Francisco", "Los Angels")
+    city_graph.add_edge_by_vertices("Los Angels", "Riverside")
+    city_graph.add_edge_by_vertices("Los Angels", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Chicago")
+    city_graph.add_edge_by_vertices("Phoenix", "Dallas")
+    city_graph.add_edge_by_vertices("Phoenix", "Houston")
+    city_graph.add_edge_by_vertices("Dallas", "Chicago")
+    city_graph.add_edge_by_vertices("Dallas", "Atlanta")
+    city_graph.add_edge_by_vertices("Dallas", "Houston")
+    city_graph.add_edge_by_vertices("Houston", "Atlanta")
+    city_graph.add_edge_by_vertices("Houston", "Miami")
+    city_graph.add_edge_by_vertices("Atlanta", "Chicago")
+    city_graph.add_edge_by_vertices("Atlanta", "Washington")
+    city_graph.add_edge_by_vertices("Atlanta", "Miami")
+    city_graph.add_edge_by_vertices("Miami", "Washington")
+    city_graph.add_edge_by_vertices("Chicago", "Detroit")
+    city_graph.add_edge_by_vertices("Detroit", "Boston")
+    city_graph.add_edge_by_vertices("Detroit", "Washington")
+    city_graph.add_edge_by_vertices("Detroit", "New York")
+    city_graph.add_edge_by_vertices("Boston", "New York")
+    city_graph.add_edge_by_vertices("New York", "Philadelphia")
+    city_graph.add_edge_by_vertices("Philadelphia", "Washington")
+    print(city_graph)
