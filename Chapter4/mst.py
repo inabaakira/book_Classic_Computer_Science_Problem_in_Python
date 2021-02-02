ec3706acd3fae1d5cf63f6f911224bafa419123f
INABA Akira <inaba.akira@gmail.com>
2021-02-02 23:26:42 +0900

    [Chapter 4] Listing 4.12. mst.py continued

--- Chapter4/mst.py
+++ Chapter4/mst.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: mst.py
 #    Created:       <2021/01/23 23:38:42>
-#    Last Modified: <2021/02/01 18:53:34>
+#    Last Modified: <2021/02/02 23:21:57>
 
 from typing import TypeVar, List, Optional
 from weighted_graph import WeightedGraph
@@ -45,3 +45,43 @@ def print_weighted_path(wg: WeightedGraph, wp: WeightedPath) -> None:
     for edge in wp:
         print(f"{wg.vertex_at(edge.u)} {edge.weight}> {wg.vertex_at(edge.v)}")
     print(f"Total weight: {total_weight(wp)}")
+
+if __name__ == "__main__":
+    city_graph2: WeightedGraph[str] = WeightedGraph([
+        "Seattle", "San Francisco", "Los Angels", "Riverside",    "Phoenix",
+        "Chicago", "Boston",        "New York",   "Atlanta",      "Miami",
+        "Dallas",  "Houston",       "Detroit",    "Philadelphia", "Washington"
+    ])
+
+    city_graph2.add_edge_by_vertices("Seattle",       "Chicago",       1737)
+    city_graph2.add_edge_by_vertices("Seattle",       "San Francisco",  678)
+    city_graph2.add_edge_by_vertices("San Francisco", "Riverside",      386)
+    city_graph2.add_edge_by_vertices("San Francisco", "Los Angels",     348)
+    city_graph2.add_edge_by_vertices("Los Angels",    "Riverside",       50)
+    city_graph2.add_edge_by_vertices("Los Angels",    "Phoenix",        357)
+    city_graph2.add_edge_by_vertices("Riverside",     "Phoenix",        307)
+    city_graph2.add_edge_by_vertices("Riverside",     "Chicago",       1704)
+    city_graph2.add_edge_by_vertices("Phoenix",       "Dallas",         887)
+    city_graph2.add_edge_by_vertices("Phoenix",       "Houston",       1015)
+    city_graph2.add_edge_by_vertices("Dallas",        "Chicago",        805)
+    city_graph2.add_edge_by_vertices("Dallas",        "Atlanta",        721)
+    city_graph2.add_edge_by_vertices("Dallas",        "Houston",        225)
+    city_graph2.add_edge_by_vertices("Houston",       "Atlanta",        702)
+    city_graph2.add_edge_by_vertices("Houston",       "Miami",          968)
+    city_graph2.add_edge_by_vertices("Atlanta",       "Chicago",        588)
+    city_graph2.add_edge_by_vertices("Atlanta",       "Washington",     543)
+    city_graph2.add_edge_by_vertices("Atlanta",       "Miami",          604)
+    city_graph2.add_edge_by_vertices("Miami",         "Washington",     923)
+    city_graph2.add_edge_by_vertices("Chicago",       "Detroit",        238)
+    city_graph2.add_edge_by_vertices("Detroit",       "Boston",         613)
+    city_graph2.add_edge_by_vertices("Detroit",       "Washington",     396)
+    city_graph2.add_edge_by_vertices("Detroit",       "New York",       482)
+    city_graph2.add_edge_by_vertices("Boston",        "New York",       190)
+    city_graph2.add_edge_by_vertices("New York",      "Philadelphia",    81)
+    city_graph2.add_edge_by_vertices("Philadelphia",  "Washington",     123)
+
+    result: Optional[WeightedPath] = mst(city_graph2)
+    if result is None:
+        print("No solution found!")
+    else:
+        print_weighted_path(city_graph2, result)
