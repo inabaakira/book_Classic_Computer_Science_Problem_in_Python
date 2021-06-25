5341c2bffe7622da8171ba881963cf0888e4b430
INABA Akira <inaba.akira@gmail.com>
2021-06-25 23:16:51 +0900

    [Chapter 5] Listing 5.3. genetic_algorithm.py continued

--- Chapter5/genetic_algorithm.py
+++ Chapter5/genetic_algorithm.py
@@ -16,3 +16,19 @@ C = TypeVar('C', bound=Chromosome)
 
 class GenericAlgorithm(Generic[C]):
     SelectionType = Enum("SelectionType", "ROULETTE TOURNAMENT")
+
+    def __init__(self,
+                 initial_population: List[C],
+                 threshold: float,
+                 max_generations: int = 100,
+                 mutation_chance: float = 0.01,
+                 crossover_chance: float = 0.7,
+                 selection_type: SelectionType.TOURNAMENT) -> None:
+        self._population: List[C] = initial_population
+        self._threshold: float = threshold
+        self._max_generations: int = max_generations
+        self._mutation_chance: float = mutation_chance
+        self._crossover_chance: float = crossover_chance
+        self._selection_type: GeneticAlgorithm.SelectionType = selection_type
+        self._fitness_key: Callable = type(self._population[0]).fitness
+
