e47d157306d31c7c05ad5a0768bb5c5db13a06a2
INABA Akira <inaba.akira@gmail.com>
2021-06-25 23:17:45 +0900

    [Chapter 5] Listing 5.4. genetic_algorithm.py continued

--- Chapter5/genetic_algorithm.py
+++ Chapter5/genetic_algorithm.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: genetic_algorithm.py
 #    Created:       <2021/06/22 11:12:37>
-#    Last Modified: <2021/06/22 11:15:29>
+#    Last Modified: <2021/06/22 16:49:50>
 
 from __future__ import annotations
 from typing import TypeVar, Generic, List, Tuple, Callable
@@ -32,3 +32,7 @@ class GenericAlgorithm(Generic[C]):
         self._selection_type: GeneticAlgorithm.SelectionType = selection_type
         self._fitness_key: Callable = type(self._population[0]).fitness
 
+    # 確率分布を使って 2 つの親を選ぶ．
+    # 注意: fitness の負の結果に対しては動作しない．
+    def _pick_roulette(self, wheel: List[float]) -> Tuple[C, C]:
+        return tuple(choices(self._population, weight=wheel, k=2))
