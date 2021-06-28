3df2ee0bc8e06698730197097a479cec607121b6
INABA Akira <inaba.akira@gmail.com>
2021-06-28 17:06:05 +0900

    [Chapter 5] Listing 5.6. genetic_algorithm.py continued

--- Chapter5/genetic_algorithm.py
+++ Chapter5/genetic_algorithm.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: genetic_algorithm.py
 #    Created:       <2021/06/22 11:12:37>
-#    Last Modified: <2021/06/25 23:21:42>
+#    Last Modified: <2021/06/28 17:05:45>
 
 from __future__ import annotations
 from typing import TypeVar, Generic, List, Tuple, Callable
@@ -32,7 +32,7 @@ class GenericAlgorithm(Generic[C]):
         self._selection_type: GeneticAlgorithm.SelectionType = selection_type
         self._fitness_key: Callable = type(self._population[0]).fitness
 
-    # 確率分布を使って 2 つの親を選ぶ．
+    # 確率分布を使って親を 2 つ選ぶ．
     # 注意: fitness の負の結果に対しては動作しない．
     def _pick_roulette(self, wheel: List[float]) -> Tuple[C, C]:
         return tuple(choices(self._population, weight=wheel, k=2))
@@ -41,3 +41,23 @@ class GenericAlgorithm(Generic[C]):
     def _pick_tournament(self, num_participants: int) -> Tuple[C, C]:
         participants: List[C] = choices(self._popilation, k=num_participants)
         return tuple(nlargest(2, participants, key=self._fitness_key))
+
+    # population を新世代の個体と取り替える．
+    def _reproduce_and_replace(self) -> None:
+        new_population: List[C] = []
+        # 新世代で置き換え尽くすまで続ける．
+        while len(new_population) < len(self._population):
+            # 親を 2 つ選ぶ。
+            if self._selection_type == GeneticAlgorithm.Selection.ROULETTE:
+                parents: Tuple[C, C] = self._pick_roulette([x.fitness() for x in self._population])
+            else:
+                parents: self._pick_tournament(len(self._population) // 2)
+            # 2 つの親が組替えを起こす場合を想定した処理。
+            if random() < self.crossover_chance:
+                new_population.extend(parents[0].crossover(parents[1]))
+            else:
+                nre_population.extend(parents)
+        # 新世代の個体が奇数の場合、余計な 1 個が含まれているのでそれを削除する。
+        if len(new_population) > len(self._population):
+            new_population.pop()
+        self._population = new_population
