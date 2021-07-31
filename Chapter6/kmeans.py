7c65148fed6288baf51409b7e6a812dca95cea24
INABA Akira <inaba.akira@gmail.com>
2021-07-31 23:15:38 +0900

    [Chapter 6] Listing 6.4. kmeans.py continued

--- Chapter6/kmeans.py
+++ Chapter6/kmeans.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: kmeans.py
 #    Created:       <2021/07/20 12:12:14>
-#    Last Modified: <2021/07/31 18:13:31>
+#    Last Modified: <2021/07/31 23:14:50>
 
 from __future__ import annotations
 from typing import TypeVar, Generic, List, Sequence
@@ -27,3 +27,19 @@ class KMeans(Generic[Point]):
     class Cluster:
         points: List[Point]
         centroid: DataPoint
+
+    def __init__(self, k: int, point: List[Point]) -> None:
+        if k < 1: # 負またはゼロ個の cluster に対して k-means は考えられない
+            raise ValueError("k must be >= 1")
+        self._points: List[Point] = points
+        self._zscore_normalize()
+        # ランダムな centroids で空の cluster を初期化する
+        self._clusters: List[KMeans.Cluster] = []
+        for _ in range(k):
+            rand_point: DataPoint = self._random_point()
+            cluster: KMeans.Cluster = KMeans.Cluster([], rand_point)
+            self._clusters.append(cluster)
+
+    @property
+    def _centroids(self) -> List[DataPoint]:
+        return [x.centroid for x in self._clusters]
