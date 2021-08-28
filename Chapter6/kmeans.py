3254fd5a0c1d41279c0c9454786bfc1550f905e9
INABA Akira <inaba.akira@gmail.com>
2021-08-28 14:50:22 +0900

    [Chapter 6] Listing 6.11. kmeans.py continued

--- Chapter6/kmeans.py
+++ Chapter6/kmeans.py
@@ -85,3 +85,27 @@ class KMeans(Generic[Point]):
                     [p.dimensions[dimension] for p in cluster.points]
                 means.append(mean(dimension_slice))
             cluster.centroid = DataPoint(means)
+
+    def run(self, max_iterations: int = 100) -> List[KMeans.Cluster]:
+        for iteration in range(max_iterations):
+            # 全ての cluster をクリアする．
+            for cluster in self._clusters:
+                cluster.points.clear()
+                # 各 point が最も近い cluster を見つけてそこに登録する．
+                self._assign_clusters()
+                old_centroids: List[DataPoint] = deepcopy(self._centroids) # 現在を保存する．
+                self._generate_centroid() # 新しい centroids を見つける．
+                if old_centroids == self._centroids: # centroids が変わらない場合
+                    print(f"Converged after {iteration} iterations")
+                    return self._clusters
+            return self._clusters
+
+
+if __name__ == "__main__":
+    point1: DataPoint = DataPoint([2.0, 1.0, 1.0])
+    point2: DataPoint = DataPoint([2.0, 2.0, 5.0])
+    point3: DataPoint = DataPoint([3.0, 1.5, 2.5])
+    kmeans_test: KMeans[DataPoint] = KMeans(2, [point1, point2, point3])
+    test_clusters: List[KMeans.Cluster] = kmeans_test.run()
+    for index, cluster in enumerate(test_clusters):
+        print(f"Cluster {index}: {cluster.points}")
