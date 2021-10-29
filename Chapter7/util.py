7fe3f0a02c399dd020bbdfbb81aa5d14f664d273
INABA Akira <inaba.akira@gmail.com>
2021-10-29 10:14:52 +0900

    [Chapter 7] Listing 7.2. util.py continued

--- Chapter7/util.py
+++ Chapter7/util.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: util.py
 #    Created:       <2021/10/28 14:21:30>
-#    Last Modified: <2021/10/28 14:23:03>
+#    Last Modified: <2021/10/29 10:01:22>
 
 from typing import list
 from math import exp
@@ -10,3 +10,11 @@ from math import exp
 # 2つのベクトルのドット積
 def dot_product(xs: List[float], ys: List[float]) -> float:
     return sum(x * y for x, y in zip(xs, ys))
+
+# the classic sigmoid activation function
+def sigmoid(x: float) -> float:
+    return 1.0 / (1.0 + exp(-x))
+
+def derivative_sigmoid(x: float) -> float:
+    sig: float = sigmoid(x)
+    return sig * (1 - sig)
