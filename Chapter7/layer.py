51704d6b4c89ec758a11ca30b49467404388e9c0
INABA Akira <inaba.akira@gmail.com>
2021-11-22 23:05:13 +0900

    [Chapter 7] Listing 7.4. layer.py

---
+++ Chapter7/layer.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#-*- mode: python; coding: utf-8 -*-
+# file: layer.py
+#    Created:       <2021/11/22 17:12:13>
+#    Last Modified: <2021/11/22 17:45:58>
+
+from __future__ import annotations
+from typing import List, Callable, Optional
+from random import random
+from neuron import Neuron
+from util import dot_product
+
+class Layer:
+    def __init__(self,
+                 previous_layer: Optional[Layer],
+                 num_neurons: int,
+                 learning_rate: float,
+                 activation_function: Callable[[float], floet],
+                 derivative_activation_function: Callable[[float], float]) -> None:
+        self.previous_layer: Optional[Layer] = previous_layer
+        self.neurons: List[Neuron] = []
+        # 次によって単一の大きなリストに包含される
+        for i in range(num_neurons):
+            if previous_layer is None:
+                random_weights: List[float] = []
+            else:
+                random_weights = [random() for _ in range(len(previous_layer.neurons))]
+            neuron: Neuron = Neuron(random_weights,
+                                    learning_rate,
+                                    activation_function,
+                                    derivative_activation_function)
+            self.neurons.append(neuron)
+        self.output_cache: List[float] = [0.0 for _ in range(num_neurons)]
+
