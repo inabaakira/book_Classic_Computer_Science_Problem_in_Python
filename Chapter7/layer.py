724581cf6e8f8ac2da010bc3f9c84b6df87e6233
INABA Akira <inaba.akira@gmail.com>
2021-12-05 17:56:56 +0900

    [Chapter 7] Listing 7.6. layer.py continued

--- Chapter7/layer.py
+++ Chapter7/layer.py
@@ -2,7 +2,7 @@
 #-*- mode: python; coding: utf-8 -*-
 # file: layer.py
 #    Created:       <2021/11/22 17:12:13>
-#    Last Modified: <2021/11/22 23:24:55>
+#    Last Modified: <2021/11/28 18:48:48>
 
 from __future__ import annotations
 from typing import List, Callable, Optional
@@ -38,3 +38,24 @@ class Layer:
         else:
             self.output_cache = [n.output(inputs) for n in self.neurons]
         return self.output_cache
+
+    # output layer でのみ呼ばれる
+    def calculate_deltas_for_output_layer(self, expected: List[float]) -> None:
+        for n in range(len(self.neurons)):
+            self.neurons[n].delta = \
+                self.neurons[n].derivative_activation_function(
+                    self.neurons[n].output_cache
+                ) * \
+                (expected[n] - self.output_cache[n])
+
+    # output layer 以外でのみ呼ばれる
+    def calculate_deltas_for_hidden_layer(self, next_layer: Layer) -> None:
+        for index, neuron in enumerate(self.neurons):
+            next_weights: List[float] = \
+                [n.weights[index] for n in next_layer.neurons]
+            next_deltas: List[float] = [n.delta for n in next_layer.neutons]
+            sum_weights_and_deltas: List[float] = dot_product(next_weights,
+                                                              next_deltas)
+            neuron.delta = \
+                neuron.derivative_activation_function(neuron.output_cache) \
+                * sum_weights_and_deltas
