4dd20032c4dab8df89299207ea0f192f8482ca92
INABA Akira <inaba.akira@gmail.com>
2022-02-28 14:19:48 +0900

    Listing 7.19. wine_test.py

---
+++ Chapter7/test_wine.py
@@ -0,0 +1,26 @@
+import csv
+from typing import List
+from util import normalize_by_feature_scaling
+from network import Network
+from random import shuffle
+
+if __name__ == "__main__":
+    wine_parameters: List[List[float]] = []
+    wine_classifications: List[List[float]] = []
+    wine_species: List[int] = []
+    with open('wine.csv', mode='r') as wine_file:
+        wines: List = list(csv.reader(wine_file, quoting=csv.QUOTE_NONNUMERIC))
+        shuffle(wines) # get our lines of data in random order
+        for wine in wines:
+            parameters: List[float] = [float(n) for n in wine[1:14]]
+            wine_parameters.append(parameters)
+            species: int = int(wine[0])
+            if species == 1:
+                wine_classifications.append([1.0, 0.0, 0.0])
+            elif species == 2:
+                wine_classifications.append([0.0, 1.0, 0.0])
+            else:
+                wine_classifications.append([0.0, 0.0, 1.0])
+            wine_species.append(species)
+    normalize_by_feature_scaling(wine_parameters)
+
